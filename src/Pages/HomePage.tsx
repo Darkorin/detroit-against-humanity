a958a15faa6c0cdb85094632a9bc41e180177050
darkorin <darkorin00@hotmail.com>
2024-11-12 10:11:04 -0700

    minor cosmetic change

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,27 +3,37 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default () => {
-    const [nickname, setNickName] = useState<string>("");
-    const navigate = useNavigate();
+  const [nickname, setNickName] = useState<string>("");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const nickname = localStorage.getItem('nickname');
-        nickname && setNickName(nickname);
-    }, []);
+  useEffect(() => {
+    const nickname = localStorage.getItem("nickname");
+    nickname && setNickName(nickname);
+  }, []);
 
-    const playGame = () => {
-        localStorage.setItem('nickname', nickname);
-        navigate('?game')
-    }
+  const playGame = () => {
+    localStorage.setItem("nickname", nickname);
+    navigate("?game");
+  };
 
-    return (
-        <div className="row offset-4">
-            <h1>Detroit Against Humanity</h1>
-            <Link to="?card-creator" style={{marginTop: 16, marginBottom: 20}}>Create Cards</Link>
-            <div className="row col-4">
-                <h4 className="col">Nickname:</h4> <input className="col" type="text" value={nickname} onChange={(e) => setNickName(e.target.value)}/>
-                <button className="col" disabled={!nickname} onClick={() => playGame()}>Play Game</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="row offset-4">
+      <h1>Detroit Against Humanity</h1>
+      <Link to="?card-creator" style={{ marginTop: 16, marginBottom: 20 }}>
+        Create Cards
+      </Link>
+      <div className="row col-4">
+        <h4 className="col-3">Nickname:</h4>
+        <input
+          className="col-4"
+          type="text"
+          value={nickname}
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <button className="col-3 offset-1" disabled={!nickname} onClick={() => playGame()}>
+          Play Game
+        </button>
+      </div>
+    </div>
+  );
 };
